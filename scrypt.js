3381964bc0b2235b6aaa99498e5ca73f6e59f637
JascSoft <173310950+JascSoft@users.noreply.github.com>
2024-08-17 21:49:07 -0700

    change const div to divParent, created new constant divChild added styles to both divs

--- scrypt.js
+++ scrypt.js
@@ -1,5 +1,11 @@
 const divContainer = document.querySelector("div#divContainer");
-const div = document.createElement("div");
+const divParent = document.createElement("div#Parent");
+const divChild = document.createElement("div#Child");
+divParent.style.cssText = "display: flex; flex-direction: column; " +
+"padding: 0px; margin: 0px;";
+divChild.style.cssText = "padding: 0px; margin: 0px;";
 let amoutOfDivs = 16;
 
+function createGrid(amoutOfDivs){
 
+}
