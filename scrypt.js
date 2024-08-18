2918a7a1fc7240a7a999e09dad3ff4b9fe360086
JascSoft <173310950+JascSoft@users.noreply.github.com>
2024-08-17 22:02:14 -0700

    corrected the const added the classes in new lines

--- scrypt.js
+++ scrypt.js
@@ -1,6 +1,8 @@
 const divContainer = document.querySelector("div#divContainer");
-const divParent = document.createElement("div#Parent");
-const divChild = document.createElement("div#Child");
+const divParent = document.createElement("div");
+const divChild = document.createElement("div");
+divChild.classList.add("child");
+divParent.classList.add("parent");
 divParent.style.cssText = "display: flex; flex-direction: column; " +
 "padding: 0px; margin: 0px;";
 divChild.style.cssText = "padding: 0px; margin: 0px;";
