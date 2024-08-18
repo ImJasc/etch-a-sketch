17b9009b3dc1d470cb49a67c78011a2babeb7da9
JascSoft <173310950+JascSoft@users.noreply.github.com>
2024-08-17 22:27:37 -0700

    created a function to append the child divs into the parents divs before created the grid

--- scrypt.js
+++ scrypt.js
@@ -8,11 +8,18 @@ divParent.style.cssText = "display: flex; flex-direction: column; " +
 divChild.style.cssText = "padding: 0px; margin: 0px; height: 100%";
 let amountOfDivs = 16;
 
-function createGrid(amount){
+function appendChilds(amount){
     for (let index = 0; index < amount; index++) {       
-        divContainer.appendChild(divParent.cloneNode(true));
+        divParent.appendChild(divChild.cloneNode(true));        
         console.log("div inserted");
     }
 }
 
+function createGrid(amount){
+    for (let index = 0; index < amount; index++) {
+        divContainer.appendChild(divParent.cloneNode(true));        
+    }
+}
+
+appendChilds(amountOfDivs);
 createGrid(amountOfDivs);
