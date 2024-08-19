31af9c02eaf37c8e326385a3d3ada2943988140e
JascSoft <173310950+JascSoft@users.noreply.github.com>
2024-08-18 19:09:56 -0700

    change the class of the nodes to ids, created the functions to change colors of the divs alter the styles of divContainer

--- scrypt.js
+++ scrypt.js
@@ -1,8 +1,8 @@
 const divContainer = document.querySelector("div#divContainer");
 const divParent = document.createElement("div");
 const divChild = document.createElement("div");
-divChild.classList.add("child");
-divParent.classList.add("parent");
+divChild.id = "child";
+divParent.id= "parent";
 divParent.style.cssText = "display: flex; flex-direction: column; " +
 "padding: 0px; margin: 0px; width: 100%; justify-content: space-around;";
 divChild.style.cssText = "padding: 0px; margin: 0px; height: 100%";
@@ -21,5 +21,17 @@ function createGrid(amount){
     }
 }
 
+function randomColor() {
+    let color = [];
+    for (let i = 0; i < 3; i++) {
+      color.push(Math.floor(Math.random() * 256));
+    }
+    return 'rgb(' + color.join(', ') + ')';
+} 
+
 appendChilds(amountOfDivs);
 createGrid(amountOfDivs);
+
+document.querySelector("div#child").addEventListener('mouseover', event => {
+    event.target.style.backgroundColor = randomColor();
+});
