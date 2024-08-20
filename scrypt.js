121b666ac92fe32535d80b70b20477a4cca405a8
JascSoft <173310950+JascSoft@users.noreply.github.com>
2024-08-19 22:03:27 -0700

    alter divParent id, created the node btnSize and added an eventlistener to ask for the new size of the grid, created a function to delete the old grid

--- scrypt.js
+++ scrypt.js
@@ -4,10 +4,10 @@ let amountOfDivs = 16;
 function createGrid(amount){
     for (let index = 0; index < amount; index++) {
         let divParent = document.createElement("div");
+        divParent.id = "parent";
         divParent.classList.add ("parent");
         divParent.style.cssText = "display: flex; flex-direction: column; " +
-        "padding: 0px; margin: 0px; width: 100%; justify-content: space-around;";
-        divParent.id = "parent#"+(index+1);
+        "padding: 0px; margin: 0px; width: 100%; justify-content: space-around;";        
         for (let index = 0; index < amount; index++) {         
             let divChild = document.createElement("div");
             divChild.classList.add ("child");
@@ -29,7 +29,32 @@ function randomColor() {
       color.push(Math.floor(Math.random() * 256));
     }
     return 'rgb(' + color.join(', ') + ')';
-} 
+}
+
+function newGrid(){    
+    divContainer.innerHTML='';
+}
 
 createGrid(amountOfDivs);
 
+const btnSize = document.querySelector("button");
+
+btnSize.addEventListener("click", event =>{
+    let newAmount = prompt("Insert a new size to the grid: ");
+    if(Number.isInteger(newAmount*1)){
+        while (newAmount > 100 || newAmount <= 0) {
+            while (newAmount > 100){
+                newAmount = prompt("The max size of the grid is 100. " + 
+                    "Please enter a new size for the grid.");
+            }
+            while (newAmount <= 0){
+                newAmount = prompt("The min size of the grid is 1. " + 
+                    "Please enter a new size for the grid.");
+            }
+        }        
+        newGrid();
+        createGrid(newAmount);
+        
+    }
+
+})
